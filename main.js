34bc2b3d65b7b1cfca7d4c65fbc220f83c65d877
Miquéias Sousa <miqueias.sousa.ti@gmail.com>
2024-12-24 10:22:28 -0300

    Atualizando main

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+      return {
+        premium: true,
+        cart: []
+      }
+    },
+    methods: {
+      updateCart(id) {
+        this.cart.push(id)
+      }
+    }
+  })
